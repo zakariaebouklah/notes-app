beea79423696275b46cd5fa357b31f5c2111ff6c
zZ7_aki <zakariae.bouklah18@gmail.com>
2022-10-17 18:16:21 +0100

    Theme Switcher Option Done.

--- src/components/Regulars/Navbar.js
+++ src/components/Regulars/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "./Button";
 import {Link} from "react-router-dom";
-import Logo from "../../images/NoteAppLogo.png"
+import Logo from "../../images/NoteAppLogo.png";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 function Navbar(props) {
 
@@ -26,13 +27,12 @@ function Navbar(props) {
         }
         else if (e.target.classList.contains("modal"))
         {
-            e.target.classList.add("hidden")
+            e.target.classList.add("hidden");
         }
-
     })
 
     return (
-        <nav className="fixed z-30 w-screen h-24 flex flex-row justify-between p-5 bg-light-one border-b-4 border-b-black">
+        <nav className="fixed z-30 w-screen h-24 flex flex-row justify-between p-5 dark:bg-dark-two bg-light-one border-b-4 border-b-black">
             <div id="logo" className="w-1/4">
                 <img className="-translate-y-5 w-20 h-20" src={Logo} alt="logo"/>
             </div>
@@ -52,9 +52,8 @@ function Navbar(props) {
                         </>
                     )
                 }
-
-
             </div>
+            <ThemeSwitcher/>
         </nav>
     );
 }
---
+++ src/components/Regulars/ThemeSwitcher.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import darkIcon from '../../icons/night-mode.png';
+import lightIcon from '../../icons/brightness.png';
+
+function ThemeSwitcher(props) {
+
+    const [isDark, setIsDark] = useState(false);
+
+    /**
+     * the first useEffect will be used to persist the icon In the Switcher
+     * and the class for html element as well
+     *
+     * the second one will be used to update the localStorage darkMode's value
+     * each time the switcher gets clicked i.e. the state gets updated
+     *
+     */
+
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem("darkMode"));
+        if (data)
+        {
+            document.querySelector("html").classList.add("dark");
+        }
+        setIsDark(data);
+    }, [] )
+
+    useEffect(() => {
+        localStorage.setItem("darkMode", JSON.stringify(isDark))
+    }, [isDark])
+
+    const handleClick = () => {
+        setIsDark(!isDark);
+        if (document.querySelector("html").classList.contains("dark"))
+        {
+            document.querySelector("html").classList.remove("dark");
+        }
+        else
+        {
+            document.querySelector("html").classList.add("dark");
+        }
+    }
+
+    return (
+        <div className="w-11 h-11 rounded-full bg-white hover:bg-dark-one border-2 border-black
+                        place-self-center shadow-inner shadow-3xl flex items-center justify-center
+                        transition-all ease-in-out duration-300 delay-75">
+            <button id="switcher" onClick={handleClick}>
+                {
+                    isDark
+                        ?
+                        <img src={darkIcon} alt="dark" className="w-7 h-7"/>
+                        :
+                        <img src={lightIcon} alt="light" className="w-7 h-7"/>
+                }
+            </button>
+        </div>
+    );
+}
+
+export default ThemeSwitcher;
